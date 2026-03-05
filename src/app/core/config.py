bba6940ddb369e27e4676a682db88773ee02f020
KarikariSamuelZachary <skarikaripresecghana@gmail.com>
2026-03-05 11:55:07 +0000

    fix: absolute path error

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -1,8 +1,10 @@
 from pydantic_settings import BaseSettings
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parents[3]
 class Settings(BaseSettings):
     APP_NAME:str = "agri-vision-ai"
     APP_VERSION:str = "1.0.0"
-    MODEL_DIR:str = "../../../ml/models"
+    MODEL_DIR:str = str(BASE_DIR / "ml" / "models")
 
 settings = Settings()
