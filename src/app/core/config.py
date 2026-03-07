88d4f9b42b16fb3e0b59783109e51c0b9f8a16ce
KarikariSamuelZachary <skarikaripresecghana@gmail.com>
2026-03-07 16:56:44 +0000

    feat: Added ALLOWED_ORIGINS env var

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -1,10 +1,13 @@
 from pydantic_settings import BaseSettings
 from pathlib import Path
+from typing import List
 
 BASE_DIR = Path(__file__).resolve().parents[3]
+
 class Settings(BaseSettings):
-    APP_NAME:str = "agri-vision-ai"
-    APP_VERSION:str = "1.0.0"
-    MODEL_DIR:str = str(BASE_DIR / "ml" / "models")
+    APP_NAME: str = "agri-vision-ai"
+    APP_VERSION: str = "1.0.0"
+    MODEL_DIR: str = str(BASE_DIR / "ml" / "models")
+    ALLOWED_ORIGINS: List[str] = ["http://localhost:3000"]
 
 settings = Settings()
